64f42917c539258a9e0ea0163c31e95b457a0766
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-29 18:04:28 +0430

    Create id.lua

---
+++ plugins/id.lua
@@ -0,0 +1,15 @@
+do function run(msg, matches)
+if matches[1]:lower() == '/id' and is_momod(msg) then
+local text = "♻️["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n♻️*Your ID* : _"..msg.from.id.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
+end
+return {
+description = "show your id",
+usage = "!id : show your userid and information",
+patterns = {
+"^/[Ii][Dd]",
+},
+run = run
+}
+end
