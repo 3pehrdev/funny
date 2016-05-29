27581921b37a0a7f68369e7e453b49437768f480
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-29 18:22:49 +0430

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -2,7 +2,8 @@ local function run(msg)
 local text = [[*Hi*〽️
 *I'm a Funny Bot*😼👊
 *Type* `/` *For Show Commands*✅
-*My Developer is *[MrBlackLife](http://telegram.me/mrblacklife)♻️]]
+*My Developer is *[MrBlackLife](http://telegram.me/mrblacklife)♻️
+[Click Here For Add Me To ur Groups](http://Telegram.me/Funn_RoBot?startgroup=new)]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
