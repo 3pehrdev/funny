e7768b09038dd66caf6b43ec313c0dfe7722322b
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-30 09:43:15 +0430

    Update mean.lua

--- plugins/mean.lua
+++ plugins/mean.lua
@@ -1,8 +1,7 @@
 local function(msg, matches)
   local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
   local data = json:decode(htp)
-	local text = '*Word* : `'..(data.data.title or data.search.q)..'`\n\n*Mean :* _'..(data.data.text or '----' )..'_\n\n*Source:* _'..(data.data.source or '----' )..'_\n\n_'..(data.error.message or '')..'_\n\n[Funny😼👊](http://telegram.me/Funn_RoBot)'
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+	return 'Word : '..(data.data.title or data.search.q)..'\n\nMean : '..(data.data.text or '----' )..'\n\nSource: '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n\nFunny😼👊\n@Funn_RoBot'
 end
 return {
   patterns = {
