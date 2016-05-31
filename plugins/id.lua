cb37067d5c150d9a477065f22a7774ae5ffb9d50
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-31 23:03:42 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,8 +1,9 @@
 do function run(msg, matches)
-if matches[1]:lower() == '/id' and is_momod(msg) then
+if matches[1]:lower() == '/id' then
 local text = "♻️["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n♻️*Your ID* : _"..msg.from.id.."_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
+reply_msg(extra.Reply, text, ok_cb, false)
 end
 return {
 description = "show your id",
