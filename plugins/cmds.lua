5634051ab2ccfb0696922688fde2f145dc23f8ab
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-01 20:15:38 +0430

    Update cmds.lua

--- plugins/cmds.lua
+++ plugins/cmds.lua
@@ -1,10 +1,8 @@
 local function run(msg)
 local text = [[1⃣  /start *Start Work Bot in Ur Groups Or Private*
-2⃣  /id *Show Your Information*
+2⃣ /id *Show Your Information*
 3⃣ /time *Show Time By Sticker*
-4⃣ /voice [text] *English Text To Voice*
-5⃣ /mean [Word] *The Meaning of The Words*
-6⃣ /tr [lang] [Text] *Translate [text] To [lang]*]]
+4⃣ /tr [lang] [Text] *Translate [text To [Lang]* ]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
