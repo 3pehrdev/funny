dcf01c6bcde573aff62daff172c958f2bfb20670
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-01 10:37:46 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -2,10 +2,9 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '♻️["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n♻️*Your ID* : _"..msg.from.id.."_'
+local text = '♻️"..msg.from.print_name.."\n @"..msg.from.username..")\n♻️Your ID : _"..msg.from.id.."_'
 if matches[1] == '/id' then
 reply_msg(reply_id, text, ok_cb, false)
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 end 
 end
