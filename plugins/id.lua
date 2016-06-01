dadda1e479d553be9e7b2fa3da4aa2b8a39c6c7d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-01 10:38:52 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '♻️"..msg.from.print_name.."\n @"..msg.from.username..")\n♻️Your ID : _"..msg.from.id.."_'
+local text = '♻️"..msg.from.print_name.."\n @"..msg.from.username..")\n♻️Your ID : "..msg.from.id..".'
 if matches[1] == '/id' then
 reply_msg(reply_id, text, ok_cb, false)
 end
