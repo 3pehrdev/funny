5fc5ed7597c631e96bef61045924492b41e77a1d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-09 02:13:37 +0430

    Update inlinekey.lua

--- plugins/inlinekey.lua
+++ plugins/inlinekey.lua
@@ -1,8 +1,12 @@
 local api_key = '171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw'
 local function run(msg,matches)
     if is_sudo(msg) then
-    local keyboard = '{"inline_keyboard":[[{"*Msg For Test*":"Test","url":"test.com"}]]}'
-    local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..msg.from.id..'&parse_mode=Markdown&text=*Msg For Test*&disable_web_page_preview=true&reply_markup='..keyboard
+    local text = URL.escape(matches[2])
+    local channel_id = matches[1]  
+  local link_text = URL.escape(matches[3])
+    local link = URL.escape(matches[4])
+    local keyboard = '{"inline_keyboard":[[{"text":"'..link_text..'","url":"'..link..'"}]]}'
+    local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..channel_id..'&parse_mode=Markdown&text='..text..'&disable_web_page_preview=true&reply_markup='..keyboard
     local dat, res = https.request(url)
       if res == 400 then
          reply_msg(msg.id, 'Error !', ok_cb, true)    
@@ -13,7 +17,7 @@ local function run(msg,matches)
   end
   return {
       patterns = {
-          "^[!/#]start"
+          "^[!/#]send (.*)+(.*)+(.*)+(.*)$"
           },
       run = run
   }
