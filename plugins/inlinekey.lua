6e39af59a9d05973a06366ddb73c331938ee2b8d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-08 21:10:39 +0430

    Update inlinekey.lua

--- plugins/inlinekey.lua
+++ plugins/inlinekey.lua
@@ -1,11 +1,19 @@
+local api_key = '171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw'
 local function run(msg,matches)
     if is_sudo(msg) then
-    local url = 'https://api.telegram.org/bot171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw/sendMessage?chat_id='..msg.from.id..'&parse_mode=Markdown&text=*test*&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"test","url":"test.com"}]]}'
+    local keyboard = '{"inline_keyboard":[[{"*Msg For Test*":"Test","url":"test.com"}]]}'
+    local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..msg.from.id..'&parse_mode=Markdown&text=*Msg For Test*&disable_web_page_preview=true&reply_markup='..keyboard
+    local dat, res = https.request(url)
+      if res == 400 then
+         reply_msg(msg.id, 'Error !', ok_cb, true)    
+         else
+         reply_msg(msg.id, 'Sent !', ok_cb, true)    
+      end
       end
   end
   return {
       patterns = {
-          "^[!/#]test$"
+          "^[!/#]start"
           },
       run = run
   }
