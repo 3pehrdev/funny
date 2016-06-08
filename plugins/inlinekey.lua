143f182017f40bacf1fc351234bec707df5c6c35
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-08 12:29:58 +0430

    Update inlinekey.lua

--- plugins/inlinekey.lua
+++ plugins/inlinekey.lua
@@ -1,7 +1,6 @@
 local function run(msg,matches)
     if is_sudo(msg) then
-    local url = 'https://api.telegram.org/bot171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw/sendMessage?chat_id='..msg.to.id..'&parse_mode=Markdown&text=*test*&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"test","url":"test.com"}]]}'
-    local dat, res = https.request(url)   
+    local url = 'https://api.telegram.org/bot171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw/sendMessage?chat_id='..msg.from.id..'&parse_mode=Markdown&text=*test*&disable_web_page_preview=true&reply_markup={"inline_keyboard":[[{"text":"test","url":"test.com"}]]}'
       end
   end
   return {
