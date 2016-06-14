9fabb0929c346cf93635621c37fdf1430c8c255e
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-14 14:40:22 +0430

    Update inlinekey.lua

--- plugins/inlinekey.lua
+++ plugins/inlinekey.lua
@@ -1,11 +1,11 @@
 local api_key = '171018888:AAFARdicFOJmb4gYlvXhqfDoNdCfuNswrxw' --توکن ربات خود را اینجا قرار دهید 
 local function run(msg,matches) 
-    local text = 'SpheroTM' 
+    local text = '*Thanks For Start Funny Bot*' 
     local channel_id = get_receiver_api(msg) 
     local link_text = 'Bot Pm Resan TM' 
     local data = load_data(_config.moderation.data) 
     local link = 'https://telegram.me/spherochbot'
-    local keyboard = '{"inline_keyboard":[[{"text":"'..link_text..'","url":"'..link..'"}],[{"text":"Join to Sphero Channel","url":"https://telegram.me/Spheroch"}],[{"text":"BlackLifeTM Channel","url":"http://telegram.me/blacklifech"},{"text":"Dev Bot","url":"http://telegram.me/mrblacklife"}]]}' --به دلخواه متن ها را تغییر دهید 
+    local keyboard = '{"inline_keyboard":[[{"text":"'..link_text..'","url":"'..link..'"}],[{"text":"Join to Sphero Channel","url":"https://telegram.me/Spheroch"}],[{"text":"BlackLife Channel","url":"http://telegram.me/blacklifech"},{"text":"Dev Bot","url":"http://telegram.me/mrblacklife"}]]}' --به دلخواه متن ها را تغییر دهید 
     local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..channel_id..'&parse_mode=Markdown&text='..text..'&disable_web_page_preview=true&reply_markup='..keyboard 
     local dat, res = https.request(url) 
       if res == 400 then 
@@ -16,7 +16,7 @@ local function run(msg,matches)
       end 
   return { 
       patterns = { 
-          "^[!/#](funny)$" 
+          "^/start" 
           }, 
       run = run 
   } 
