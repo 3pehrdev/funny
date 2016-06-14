77d3dc4660b80b62d3df4e0f1aa5bcc47e81820f
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-14 14:37:37 +0430

    Update inlinekey.lua

--- plugins/inlinekey.lua
+++ plugins/inlinekey.lua
@@ -4,7 +4,7 @@ local function run(msg,matches)
     local channel_id = get_receiver_api(msg) 
     local link_text = 'Bot Pm Resan TM' 
     local data = load_data(_config.moderation.data) 
-    local link = https://telegram.me/spherochbot
+    local link = 'https://telegram.me/spherochbot'
     local keyboard = '{"inline_keyboard":[[{"text":"'..link_text..'","url":"'..link..'"}],[{"text":"Join to Sphero Channel","url":"https://telegram.me/Spheroch"}],[{"text":"BlackLifeTM Channel","url":"http://telegram.me/blacklifech"},{"text":"Dev Bot","url":"http://telegram.me/mrblacklife"}]]}' --به دلخواه متن ها را تغییر دهید 
     local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..channel_id..'&parse_mode=Markdown&text='..text..'&disable_web_page_preview=true&reply_markup='..keyboard 
     local dat, res = https.request(url) 
